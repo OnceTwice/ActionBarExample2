bdd78d53469bb684a5df66f0a423398be3ec46de
cho <you-smile@nate.com>
2016-11-29 20:23:31 +0900

    1

---
+++ app/src/main/java/com/bit2016/actionbarexample2/ActionItemsActivity.java
@@ -0,0 +1,36 @@
+package com.bit2016.actionbarexample2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ActionItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_action_items );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // inflate menu resource(xml)
+        // getMenuInflater().inflate(R.menu.menu_main, menu);
+        // return true;
+
+        menu.add("Save")
+                .setIcon( android.R.drawable.ic_menu_edit )
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+
+        menu.add("Search")
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+
+        menu.add("Refresh")
+                .setIcon( android.R.drawable.ic_popup_sync )
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+
+        return true;
+    }
+}
---
+++ app/src/main/java/com/bit2016/actionbarexample2/ActionSearchActivity.java
@@ -0,0 +1,50 @@
+package com.bit2016.actionbarexample2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ActionSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView( R.layout.activity_action_search );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.
+                add( Menu.NONE, 0, 0, "Search").
+                setIcon( android.R.drawable.ic_menu_search ).
+                setActionView( R.layout.collapsible_edittext ).
+                setShowAsAction( MenuItem.SHOW_AS_ACTION_ALWAYS | MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW );
+
+        MenuItem menuItem = menu.getItem(0);
+        final EditText editText = (EditText)menuItem.getActionView();
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    String keyword = editText.getText().toString();
+                    Log.d("--------->", keyword);
+                    editText.setText("");
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+        return true;
+    }
+
+
+}
